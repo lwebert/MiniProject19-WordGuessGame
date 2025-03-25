96ef0d184e3ab58d5a5008a50dcbdf2395d5815e
lwebert <lauren@weberts.org>
2025-03-24 21:08:36 -0500

    Test fix for __dirname not defined in ES module scope error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import path from 'node:path';
 
+//My fix for error "__dirname" is not defined in ES module scope
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
